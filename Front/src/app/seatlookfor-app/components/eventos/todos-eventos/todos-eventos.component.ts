961622bf3795cecef6016fd05f6d5331269d105e
toniipower <antoniohl90@gmail.com>
2025-04-29 21:30:40 +0200

    creando componentes y modelos

---
+++ Front/src/app/seatlookfor-app/components/eventos/todos-eventos/todos-eventos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todos-eventos',
+  imports: [],
+  templateUrl: './todos-eventos.component.html',
+  styleUrl: './todos-eventos.component.css'
+})
+export class TodosEventosComponent {
+
+}
---
+++ Front/src/app/seatlookfor-app/seatlookfor-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seatlookfor-app',
+  imports: [],
+  templateUrl: './seatlookfor-app.component.html',
+  styleUrl: './seatlookfor-app.component.css'
+})
+export class SeatlookforAppComponent {
+
+}
