a8fe63b9fac5d8360732050a50fb7deec8f32af2
Francisco Jiménez López <paco.mlga11@gmail.com>
2025-04-30 22:34:37 +0200

    Rutas

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventoController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+Route::get('/api/eventos', [EventoController::class, 'index']);
+Route::get('/api/eventos/{id}', [EventoController::class, 'show']);
 
 require __DIR__.'/auth.php';
