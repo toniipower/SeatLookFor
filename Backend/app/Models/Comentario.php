7f4b8ebcbcb2e073a9b52e381461c701fd9772dc
Francisco Jiménez López <paco.mlga11@gmail.com>
2025-06-05 18:40:00 +0200

    comentarios

--- Backend/app/Models/Comentario.php
+++ Backend/app/Models/Comentario.php
@@ -29,4 +29,11 @@ class Comentario extends Model
         'idAsi',
     ];
 
+    public function usuario(){
+        return $this->belongsTo(Usuario::class, 'idUsu');
+    }
+    public function asiento(){
+         return $this->belongsTo(Asiento::class, 'idAsi');
+    }
+
 }
