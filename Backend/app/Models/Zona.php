efbcdad175a7b06dfc4caf748f5fb4920a82ff96
Francisco Jiménez López <paco.mlga11@gmail.com>
2025-06-09 15:00:04 +0200

    Update Zona.php

--- Backend/app/Models/Zona.php
+++ Backend/app/Models/Zona.php
@@ -4,26 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class zona extends Model
+class Zona extends Model
 {
     protected $table = 'zona';
-
-    // Clave primaria 
     protected $primaryKey = 'idZona';
-     public $timestamps = false;
-    protected $fillable=
-    [
-       
-        "estado",
-        "zona"
+    public $timestamps = false;
 
+    protected $fillable = ['nombre', 'idEst'];
 
-    ];
-
-
+    public function establecimiento()
+    {
+        return $this->belongsTo(Establecimiento::class, 'idEst', 'idEst');
+    }
 
     public function eventos()
     {
-        return $this->belongsToMany(Evento::class, 'zona-eventos', 'idZona', 'idEve');
+        return $this->belongsToMany(Evento::class, 'zona_evento', 'idZona', 'idEve');
     }
 }
